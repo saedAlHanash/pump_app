1d95e29b67655f20b162f5f077f5f55dc22a5c84
saed <samersame404@gmail.com>
2024-02-12 13:40:17 +0300

    final

--- lib/features/form/ui/widget/table_widget.dart
+++ lib/features/form/ui/widget/table_widget.dart
@@ -55,34 +55,103 @@ class _TableWidgetState extends State<TableWidget> {
         30.0.verticalSpace,
         Container(
           constraints: BoxConstraints(minHeight: 100.0.h, maxHeight: 1.0.sh),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                ...(widget.q.answer?.answers ?? []).mapIndexed((i, e) {
-                  return Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          children: e.map((e1) => e1.getTableAnswerWidget).toList()
-                            ..add(const Divider()),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () =>
-                            setState(() => widget.q.answer?.answers.removeAt(i)),
-                        icon: const ImageMultiType(
-                          url: Icons.remove_circle,
-                          color: Colors.red,
-                        ),
-                      )
-                    ],
-                  );
-                }),
-              ],
-            ),
+          child: ListView.builder(
+            itemCount: (widget.q.answer?.answers ?? []).length,
+            itemBuilder: (_, i) {
+              final e = widget.q.answer?.answers[i];
+              if (e == null) return 0.0.verticalSpace;
+
+              final itemWidget = _TableItemWidget(
+                list: e,
+                q: widget.q,
+                onDelete: () {
+                  setState(() => widget.q.answer?.answers.removeAt(i));
+                },
+              );
+
+              return itemWidget;
+            },
           ),
         ),
       ],
+    );
+  }
+}
+
+class _TableItemWidget extends StatefulWidget {
+  const _TableItemWidget(
+      {super.key, required this.list, required this.q, required this.onDelete});
+
+  final List<Questions> list;
+  final Questions q;
+
+  final Function() onDelete;
+
+  @override
+  State<_TableItemWidget> createState() => _TableItemWidgetState();
+}
+
+class _TableItemWidgetState extends State<_TableItemWidget> {
+  var key = GlobalKey();
+  Size? redboxSize;
+
+  bool isOffstage = true;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _getWidgetHeight();
+    });
+    super.initState();
+  }
+
+  void _getWidgetHeight() {
+    // Future.delayed(
+    //   const Duration(milliseconds: 100),
+    //   () {
+    if (redboxSize == null) {
+      final renderBox = key.currentContext?.findRenderObject() as RenderBox;
+      redboxSize = renderBox.size;
+      setState(() => isOffstage = false);
+    }
+    //   },
+    // );
+  }
+
+  Size getRedBoxSize(BuildContext context) {
+    final box = context.findRenderObject() as RenderBox;
+    return box.size;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Offstage(
+      offstage: isOffstage,
+      child: Builder(builder: (context) {
+
+        return SizedBox(
+          height: redboxSize?.height,
+          child: Row(
+            key: key,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: widget.list.map((e1) => e1.getTableAnswerWidget).toList()
+                    ..add(const Divider()),
+                ),
+              ),
+              IconButton(
+                onPressed: () => widget.onDelete.call(),
+                icon: const ImageMultiType(
+                  url: Icons.remove_circle,
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+        );
+      }),
     );
   }
 }
